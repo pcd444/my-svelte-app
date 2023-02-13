4623231f1a25fefd3a5f6f9045c3a6840aec5736
Peter C Duffy <peter.clements.duffy@gmail.com>
2023-02-13 03:41:52 -0500

    Made todo basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { each } from "svelte/internal";
+
+
+    let tasks = [{text:'Walk dog', done:false},{text:'Walk cat', done:false},{text:'Walk friend', done:false}];
+    function addTask(){
+        tasks = tasks.concat({text:'', done:false});
+    }
+    function clearDoneTasks(){
+        tasks = tasks.filter(v=>!v.done);
+    }
+</script>
+<h1>TODO app</h1>
+
+{#each tasks as {text,done}}
+    <div class:done = {done}>
+        <input type="checkbox" bind:checked={done}>
+        <input bind:value={text}>
+    </div>
+{/each}
+<button on:click={addTask}> Add task</button>
+<button on:click={clearDoneTasks}> Clear completed</button>
+
+<style>
+    .done{
+        opacity: .6;;
+    }
+</style>
